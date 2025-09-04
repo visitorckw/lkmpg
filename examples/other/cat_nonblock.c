91b8416f960dbb2144d0a5a2cd7be0ab69d10f51
Kuan-Wei Chiu <visitorckw@gmail.com>
2025-09-05 01:35:19 +0800

    cat_nonblock: convert spaces to tab

--- examples/other/cat_nonblock.c
+++ examples/other/cat_nonblock.c
@@ -12,49 +12,49 @@
 
 int main(int argc, char *argv[])
 {
-    int fd; /* The file descriptor for the file to read */
-    size_t bytes; /* The number of bytes read */
-    char buffer[MAX_BYTES]; /* The buffer for the bytes */
+	int fd; /* The file descriptor for the file to read */
+	size_t bytes; /* The number of bytes read */
+	char buffer[MAX_BYTES]; /* The buffer for the bytes */
 
-    /* Usage */
-    if (argc != 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
-        puts("Reads the content of a file, but doesn't wait for input");
-        exit(EXIT_FAILURE);
-    }
+	/* Usage */
+	if (argc != 2) {
+		printf("Usage: %s <filename>\n", argv[0]);
+		puts("Reads the content of a file, but doesn't wait for input");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Open the file for reading in non blocking mode */
-    fd = open(argv[1], O_RDONLY | O_NONBLOCK);
+	/* Open the file for reading in non blocking mode */
+	fd = open(argv[1], O_RDONLY | O_NONBLOCK);
 
-    /* If open failed */
-    if (fd == -1) {
-        puts(errno == EAGAIN ? "Open would block" : "Open failed");
-        exit(EXIT_FAILURE);
-    }
+	/* If open failed */
+	if (fd == -1) {
+		puts(errno == EAGAIN ? "Open would block" : "Open failed");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Read the file and output its contents */
-    do {
-        /* Read characters from the file */
-        bytes = read(fd, buffer, MAX_BYTES);
+	/* Read the file and output its contents */
+	do {
+		/* Read characters from the file */
+		bytes = read(fd, buffer, MAX_BYTES);
 
-        /* If there's an error, report it and die */
-        if (bytes == -1) {
-            if (errno == EAGAIN)
-                puts("Normally I'd block, but you told me not to");
-            else
-                puts("Another read error");
-            exit(EXIT_FAILURE);
-        }
+		/* If there's an error, report it and die */
+		if (bytes == -1) {
+			if (errno == EAGAIN)
+				puts("Normally I'd block, but you told me not to");
+			else
+				puts("Another read error");
+			exit(EXIT_FAILURE);
+		}
 
-        /* Print the characters */
-        if (bytes > 0) {
-            for (int i = 0; i < bytes; i++)
-                putchar(buffer[i]);
-        }
+		/* Print the characters */
+		if (bytes > 0) {
+			for (int i = 0; i < bytes; i++)
+				putchar(buffer[i]);
+		}
 
-        /* While there are no errors and the file isn't over */
-    } while (bytes > 0);
+		/* While there are no errors and the file isn't over */
+	} while (bytes > 0);
 
-    close(fd);
-    return 0;
+	close(fd);
+	return 0;
 }
